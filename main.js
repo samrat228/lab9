3b936dd6bacdd543c44dac4050714d5f86d1887a
Самрат Абенов <samrat.abenov@nu.edu.kz>
2022-10-25 22:08:48 +0600

    ff

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-let a=5
-let b=7
-let p=2*(a+b)
-console.log(p)
-
-let s=(a*a) + (b*b)
-console.log(s)
-
-if (p > 0) {
-console.log("p is positive")
+function perimeter(a, b){
+    let p=2*(a+b)
+    console.log(p)
 }
 
-else if (p==0) {
-console.log("p is 0")
+perimeter(5, 7)
+
+function sumSquares(a, b){
+    let s=(a*a) + (b*b)
+    console.log(s)
 }
 
-else { 
-Console.log("p is negative")
+sumSquares(5, 7)
+
+function checkNumber(n){
+    if (n > 0) {
+    console.log("The given number:", n, " is positive")
+    }
+
+    else if (n==0) {
+    console.log("The given number:", n, " is 0")
+    }
+
+    else { 
+    console.log("The given number:", n, " is negative")
+        }
 }
+
+checkNumber(-1)
